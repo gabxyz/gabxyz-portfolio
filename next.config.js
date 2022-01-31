d809740816aba7db392c4c449ebd687758fae0fa
Gabriel <gabxyzdev@gmail.com>
2022-01-31 10:34:44 -0300

    Install dependencies and made some changes to config files Add react-icons for the page icons and next-themes to add darkmode functionality Add tailwind theme properties in tailwind.config.js and enable darkmode class functionality Add nextjs config for locales and trailing slashes in next.config.js Add ts config to change the basePath in tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  poweredByHeader: false,
+  trailingSlash: true,
+  i18n: {
+    locales: ['en-US', 'pt-BR'],
+    defaultLocale: 'en-US',
+  },
   reactStrictMode: true,
 }
